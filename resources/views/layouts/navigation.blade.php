0d93d8a4f9dd73d7e24b655e0d182302a5021802
Wilimber Rico <ricowilimber16@gmail.com>
2024-06-26 13:07:05 -0400

    Traduccion de algunos Formularios faltantes  de Ingles a Españos en el apartado del perfil

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,11 +16,11 @@
                         {{ __('Pagina Principal') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+               {{-- <div div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
                         {{ __('List Users') }}
                     </x-nav-link>
-                </div>
+                </div> --}}
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('proveedor.index')" :active="request()->routeIs('proveedor.index')">
                         {{ __('Proveedores') }}
--- resources/views/proveedor/index.blade.php
+++ resources/views/proveedor/index.blade.php
@@ -1,9 +1,9 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('List Proveedor') }}
+            {{ __('Lista Proveedor') }}
         </h2>
-        <a type="button" href="{{ route('proveedor.create') }}" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">crear</a>
+        <a type="button" href="{{ route('proveedor.create') }}" class="rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">crear</a>
     </x-slot>
 <div class="relative overflow-x-auto">
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
